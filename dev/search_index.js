b65df20aa381858c2fff9125e6c5e5c2142eb44e
Documenter.jl <documenter@juliadocs.github.io>
2022-03-29 23:11:42 +0000

    build based on 25fc6cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultiDimDictionaries","category":"page"},{"location":"#MultiDimDictionaries","page":"Home","title":"MultiDimDictionaries","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultiDimDictionaries.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultiDimDictionaries]","category":"page"}]
+}
